bad2bd8ecbfa63646630af976a232b828eb831ae
Jelle Luteijn <jelle@luteijn.pro>
2016-04-21 16:11:18 +0200

    itemName doorvoeren

--- java/src/main/java/org/joyofcoding/objectcalisthenics/GildedRose.java
+++ java/src/main/java/org/joyofcoding/objectcalisthenics/GildedRose.java
@@ -25,10 +25,10 @@ public class GildedRose {
 
 			String itemName = item.getName();
 
-			if (!item.getName().equals("Aged Brie")
-					&& !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+			if (!itemName.equals("Aged Brie")
+					&& !itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
 				if (item.getQuality() > 0) {
-					if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+					if (!itemName.equals("Sulfuras, Hand of Ragnaros")) {
 						item.setQuality(item.getQuality() - 1);
 					}
 				}
@@ -36,7 +36,7 @@ public class GildedRose {
 				if (item.getQuality() < 50) {
 					item.setQuality(item.getQuality() + 1);
 
-					if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+					if (itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
 						if (item.getSellIn() < 11) {
 							if (item.getQuality() < 50) {
 								item.setQuality(item.getQuality() + 1);
@@ -52,15 +52,15 @@ public class GildedRose {
 				}
 			}
 
-			if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+			if (!itemName.equals("Sulfuras, Hand of Ragnaros")) {
 				item.setSellIn(item.getSellIn() - 1);
 			}
 
 			if (item.getSellIn() < 0) {
-				if (!item.getName().equals("Aged Brie")) {
-					if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+				if (!itemName.equals("Aged Brie")) {
+					if (!itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
 						if (item.getQuality() > 0) {
-							if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+							if (!itemName.equals("Sulfuras, Hand of Ragnaros")) {
 								item.setQuality(item.getQuality() - 1);
 							}
 						}
